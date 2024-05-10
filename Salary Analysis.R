09d3b0d3f22e407d10f55160a7448fe21fc02a7b
Savindu <savinduruhunuhewa@gmail.com>
2024-05-10 18:32:55 +0530

    Fill the null values

--- Salary Analysis.R
+++ Salary Analysis.R
@@ -12,3 +12,28 @@ print(head(salary))
 
 ## get the data types and number of objects
 str(salary)
+
+## attatch the dataset
+attach(salary)
+
+## view the dataset again
+View(salary)
+
+## check the null values
+salary[!complete.cases(salary),]
+
+## install the Zoo package
+install.packages("zoo")
+
+## call the zoo library
+library("zoo")
+
+## filling the null values
+salary$location_latitude  <- na.fill(location_latitude, 122.09)
+salary$location_longitude <- na.fill(location_longitude, 100.02)
+View(salary)
+## Normality Testing
+
+## Anderson darling testing
+
+ad.test()
